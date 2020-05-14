24103f04f225c045340787d1bad431e96e75addf
LashmanoW <62896643+LashmanoW@users.noreply.github.com>
2020-05-15 00:04:13 +0300

    Ready

---
+++ LW21/2/2.pas
@@ -0,0 +1,101 @@
+PROGRAM Encryption(INPUT, OUTPUT);
+{Переводит символы из INPUT в код согласно Chiper 
+  и печатает новые символы в OUTPUT}
+CONST
+  Len = 20;
+TYPE
+  Str = ARRAY [1 .. Len] OF 'A' .. 'Z';
+  Chiper = ARRAY ['A' .. 'Z'] OF CHAR;
+  MsgLen = 0 .. Len;
+VAR
+  Msg: Str;
+  Code: Chiper;
+  I: MsgLen;
+  StrLength: MsgLen;
+  Error: BOOLEAN;
+
+PROCEDURE Initialize(VAR Code: Chiper; VAR Error: BOOLEAN);
+{Присвоить Code шифр замены}
+VAR  
+  InitializeFile: TEXT;
+  W1, W2, W3: CHAR;
+  UserChar: SET OF CHAR;
+BEGIN {Initialize}  
+  ASSIGN(InitializeFile, 'Initialize.txt');
+  RESET(InitializeFile);
+  UserChar := [];
+  WHILE NOT EOF(InitializeFile) AND NOT Error
+  DO
+    BEGIN
+      WHILE NOT EOLN(InitializeFile)
+      DO
+        BEGIN
+          W1 := W2;
+          W2 := W3;
+          READ(InitializeFile, W3)
+        END;      
+      IF ((W1 IN ['A' .. 'Z']) AND (W2 = '=') AND (NOT(W3 IN UserChar)))
+      THEN
+        BEGIN
+          UserChar := UserChar + [W3];
+          Code[W1] := W3  
+        END
+      ELSE
+        BEGIN
+          Error := TRUE;
+          WRITELN(OUTPUT, ' Error: invalid values')
+        END;
+      READLN(InitializeFile)   
+    END;
+  CLOSE(InitializeFile)  
+END;  {Initialize}
+ 
+PROCEDURE Encode(VAR S: Str; StrLength: MsgLen);
+{Выводит символы из Code, соответствующие символам из S}
+VAR
+  Index: MsgLen;
+BEGIN {Encode}
+  FOR Index := 1 TO StrLength
+  DO
+    IF S[Index] IN ['A' .. 'Z']
+    THEN
+      WRITE(OUTPUT, Code[S[Index]])
+    ELSE
+      IF S[Index] = ' '
+      THEN
+        WRITE(OUTPUT, '_') 
+      ELSE  
+        WRITE(OUTPUT, S[Index]);
+  WRITELN(OUTPUT);        
+END;  {Encode}
+ 
+BEGIN {Encryption}
+  {Инициализировать Code}
+  Error := FALSE;
+  Initialize(Code, Error);  
+  WHILE NOT Error AND NOT EOF(INPUT)
+  DO
+    BEGIN
+      {читать строку в Msg и распечатать ее}
+      I := 0;
+      StrLength := I;
+      WHILE NOT EOLN(INPUT) AND (I < Len)
+      DO
+        BEGIN
+          I := I + 1;
+          StrLength := I;
+          READ(INPUT, Msg[I]);
+          WRITE(OUTPUT, Msg[I])
+        END;
+      READLN(INPUT);        
+      WRITELN(OUTPUT);
+      {распечатать кодированное сообщение}
+      IF I <> 0
+      THEN
+        Encode(Msg, StrLength)
+      ELSE
+        WRITELN(OUTPUT);
+      WRITELN(OUTPUT, '{ Message length is ', StrLength, ' }')      
+    END    
+END.  {Encryption}
+
