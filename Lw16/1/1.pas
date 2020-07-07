c004869f573871feef2ac538631e4dfbee62668b
LashmanoW <62896643+LashmanoW@users.noreply.github.com>
2020-07-07 23:25:01 +0300

    Ready

---
+++ Lw16/1/1.pas
@@ -0,0 +1,30 @@
+PROGRAM SortMonth(INPUT, OUTPUT);
+USES
+  DateIO;
+VAR
+  Mo1, Mo2: Month;
+BEGIN
+  ReadMonth(INPUT, Mo1);
+  ReadMonth(INPUT, Mo2);
+  IF (Mo1 = NoMonth) OR (Mo2 = NoMonth)
+  THEN
+    WRITELN('¬ходные данные записаны неверно')
+  ELSE
+    IF Mo1 = Mo2
+    THEN
+      BEGIN
+        WRITELN(OUTPUT, 'ќба мес€ца ');
+        WriteMonth(OUTPUT, Mo2)
+      END
+    ELSE
+      BEGIN
+        WriteMonth(OUTPUT, Mo1);
+        IF Mo1 < Mo2
+        THEN
+          WRITE(OUTPUT, ' предшествует ')
+        ELSE
+          WRITE(OUTPUT, ' следует за ');
+        WriteMonth(OUTPUT, Mo2);
+        WRITELN(OUTPUT)
+      END    
+END.
---
+++ Lw16/2/1.pas
@@ -0,0 +1,40 @@
+PROGRAM SarahRevere(INPUT, OUTPUT); 
+VAR
+  W1, W2, W3, W4: CHAR;
+  Land, Sea: BOOLEAN; 
+ 
+BEGIN {SarahRevere}   
+  {Инициализация} 
+  Land := FALSE;
+  Sea := FALSE;  
+  IF NOT EOLN
+  THEN
+    READ(W4);
+  W3 := W4;
+  W2 := W3;
+  W1 := W2;  
+  WHILE NOT EOLN AND NOT (Land OR Sea)   
+  DO
+    BEGIN
+      {движение окна}
+      W1 := W2;
+      W2 := W3;
+      W3 := W4;
+      READ(W4);           
+      {проверка окна на land}
+      Land := (W1 = 'l') AND (W2 = 'a') AND (W3 = 'n') AND (W4 = 'd');
+      {проверка окна на sea}
+      Sea := (W2 = 's') AND (W3 = 'e') AND (W4 = 'a')
+    END;
+  {создание сообщения Sarah}
+  IF Land
+  THEN
+    WRITELN('The Britsish are coming by land')
+  ELSE
+    IF Sea
+    THEN
+      WRITELN('The British are coming by sea')
+    ELSE
+      WRITELN('Sarah didn''t say')
+END.  {SarahRevere} 
+
