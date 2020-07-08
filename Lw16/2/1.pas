b2316c05d0c843a8f1a590748d1e698fd7526b4a
LashmanoW <62896643+LashmanoW@users.noreply.github.com>
2020-07-08 22:23:29 +0300

    Ready

--- Lw16/2/1.pas
+++ Lw16/2/1.pas
@@ -2,7 +2,38 @@ PROGRAM SarahRevere(INPUT, OUTPUT);
 VAR
   W1, W2, W3, W4: CHAR;
   Land, Sea: BOOLEAN; 
- 
+  
+PROCEDURE WindowMove(VAR W1, W2, W3, W4: CHAR);
+BEGIN
+  W1 := W2;
+  W2 := W3;
+  W3 := W4;
+  READ(W4);    
+END; 
+
+PROCEDURE CheckLand(VAR Land: BOOLEAN; VAR W1, W2, W3, W4: CHAR);
+BEGIN
+  Land := (W1 = 'l') AND (W2 = 'a') AND (W3 = 'n') AND (W4 = 'd')
+END;
+
+PROCEDURE CheckSea(VAR Land: BOOLEAN; VAR W2, W3, W4: CHAR);
+BEGIN
+  Sea := (W2 = 's') AND (W3 = 'e') AND (W4 = 'a')
+END;
+
+PROCEDURE WriteMessage(VAR Land, Sea: BOOLEAN);
+BEGIN
+  IF Land
+  THEN
+    WRITELN('The Britsish are coming by land')
+  ELSE
+    IF Sea
+    THEN
+      WRITELN('The British are coming by sea')
+    ELSE
+      WRITELN('Sarah didn''t say')  
+END;
+
 BEGIN {SarahRevere}   
   {Инициализация} 
   Land := FALSE;
@@ -17,24 +48,13 @@ BEGIN {SarahRevere}
   DO
     BEGIN
       {движение окна}
-      W1 := W2;
-      W2 := W3;
-      W3 := W4;
-      READ(W4);           
+       WindowMove(W1, W2, W3, W4);   
       {проверка окна на land}
-      Land := (W1 = 'l') AND (W2 = 'a') AND (W3 = 'n') AND (W4 = 'd');
+       CheckLand(Land, W1, W2, W3, W4);
       {проверка окна на sea}
-      Sea := (W2 = 's') AND (W3 = 'e') AND (W4 = 'a')
+       CheckSea(Land, W2, W3, W4)
     END;
   {создание сообщения Sarah}
-  IF Land
-  THEN
-    WRITELN('The Britsish are coming by land')
-  ELSE
-    IF Sea
-    THEN
-      WRITELN('The British are coming by sea')
-    ELSE
-      WRITELN('Sarah didn''t say')
+  WriteMessage(Land, Sea) 
 END.  {SarahRevere} 
 
